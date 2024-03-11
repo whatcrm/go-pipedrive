468bc5b53cf8d380601dca2bf81f99f737e47761
Aisha Kozhamseitova <kozhamseitova91@gmail.com>
2024-03-11 17:04:40 +0500

    added endpoints

--- utils/api.go
+++ utils/api.go
@@ -1,2 +1,9 @@
 package utils
 
+const (
+	BaseURL   = "https://api.pipedrive.com/v1"
+	WebhooksEndPoint  = "/v1/webhooks"
+	OAuthURLEndPoint  = "https://oauth.pipedrive.com/oauth"
+	AuthorizeEndPoint = "authorize"
+	TokenEndPoint     = "token"
+)
