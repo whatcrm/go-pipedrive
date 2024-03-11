fa60bf91e1f26f497168f28764c1ad54ac559fc9
Aisha Kozhamseitova <kozhamseitova91@gmail.com>
2024-03-11 17:28:22 +0500

    added constants

--- utils/api.go
+++ utils/api.go
@@ -1,9 +1,10 @@
 package utils
 
 const (
-	BaseURL   = "https://api.pipedrive.com/v1"
-	WebhooksEndPoint  = "/v1/webhooks"
-	OAuthURLEndPoint  = "https://oauth.pipedrive.com/oauth"
-	AuthorizeEndPoint = "authorize"
-	TokenEndPoint     = "token"
+	BaseURL                = "https://api.pipedrive.com/v1"
+	WebhooksEndPoint       = "/webhooks?api_token=%s"
+	DeleteWebhooksEndPoint = "/webhooks:id?api_token==%s"
+	OAuthURLEndPoint       = "https://oauth.pipedrive.com/oauth"
+	AuthorizeEndPoint      = "authorize?client_id=%s&redirect_uri=%s&state=%s"
+	TokenEndPoint          = "token"
 )
