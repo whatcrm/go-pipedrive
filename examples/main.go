a3f60f7d0d0e34b5093d15c008ba5cffb601c4a5
Aisha Kozhamseitova <kozhamseitova91@gmail.com>
2024-03-19 15:21:12 +0500

    added example of creating new client and auth

---
+++ examples/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	gopipedrive "github.com/whatcrm/go-pipedrive"
+)
+
+func main() {
+	clientID := "<YOUR_CLIENT_ID>"
+	clientSecret := "<YOUR_CLIENT_SECRET>"
+
+	authorizationCode := "<YOUR_AUTH_CODE>"
+	redirectURI := "<YOUR_REDIRECT_URI>"
+
+	domain := "<YOUR_COMPANY_DOMAIN>"
+
+	client, err := gopipedrive.NewClient(domain, clientID, clientSecret, redirectURI)
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	ctx := context.Background()
+
+	token, err := client.GetAccessToken(ctx, authorizationCode)
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	fmt.Println("result: ", token)
+}
