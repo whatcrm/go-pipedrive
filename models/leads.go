0e885b26a7cf7c1e38d9a7b6c79e136e329cdee8
Aisha Kozhamseitova <kozhamseitova91@gmail.com>
2024-08-22 10:55:04 +0500

    fix type of labelid

--- models/leads.go
+++ models/leads.go
@@ -1,32 +1,32 @@
 package models
 
 type Lead struct {
-	ID                 string  `json:"id"`
-	Title              string  `json:"title"`
-	OwnerID            int     `json:"owner_id"`
-	CreatorID          int     `json:"creator_id"`
-	PersonID           int     `json:"person_id,omitempty"`
-	OrganizationID     int     `json:"organization_id,omitempty"`
-	SourceName         string  `json:"source_name"`
-	Origin             string  `json:"origin"`
-	OriginID           string  `json:"origin_id,omitempty"`
-	Channel            int     `json:"channel"`
-	ChannelID          string  `json:"channel_id,omitempty"`
-	IsArchived         bool    `json:"is_archived"`
-	WasSeen            bool    `json:"was_seen"`
-	Amount             float64 `json:"amount,omitempty"`
-	Currency           string  `json:"currency,omitempty"`
-	ExpectedCloseDate  string  `json:"expected_close_date,omitempty"`
-	NextActivityID     int     `json:"next_activity_id,omitempty"`
-	AddTime            string  `json:"add_time"`
-	UpdateTime         string  `json:"update_time"`
-	VisibleTo          string  `json:"visible_to"`
-	CCEmail            string  `json:"cc_email"`
+	ID                string  `json:"id"`
+	Title             string  `json:"title"`
+	OwnerID           int     `json:"owner_id"`
+	CreatorID         int     `json:"creator_id"`
+	PersonID          int     `json:"person_id,omitempty"`
+	OrganizationID    int     `json:"organization_id,omitempty"`
+	SourceName        string  `json:"source_name"`
+	Origin            string  `json:"origin"`
+	OriginID          string  `json:"origin_id,omitempty"`
+	Channel           int     `json:"channel"`
+	ChannelID         string  `json:"channel_id,omitempty"`
+	IsArchived        bool    `json:"is_archived"`
+	WasSeen           bool    `json:"was_seen"`
+	Amount            float64 `json:"amount,omitempty"`
+	Currency          string  `json:"currency,omitempty"`
+	ExpectedCloseDate string  `json:"expected_close_date,omitempty"`
+	NextActivityID    int     `json:"next_activity_id,omitempty"`
+	AddTime           string  `json:"add_time"`
+	UpdateTime        string  `json:"update_time"`
+	VisibleTo         string  `json:"visible_to"`
+	CCEmail           string  `json:"cc_email"`
 }
 
 type LeadResponse struct {
-	Success bool  `json:"success"`
-	Data    Lead  `json:"data"`
+	Success bool `json:"success"`
+	Data    Lead `json:"data"`
 }
 
 type LeadsResponse struct {
@@ -35,8 +35,8 @@ type LeadsResponse struct {
 }
 
 type PermittedUsersResponse struct {
-	Success bool   `json:"success"`
-	Data    []int  `json:"data"`
+	Success bool  `json:"success"`
+	Data    []int `json:"data"`
 }
 
 type LeadSearchResponse struct {
@@ -45,37 +45,37 @@ type LeadSearchResponse struct {
 }
 
 type LeadRequest struct {
-	Title             string  `json:"title"`
-	OwnerID           int     `json:"owner_id,omitempty"`
-	LabelIDs          []int   `json:"label_ids,omitempty"`
-	PersonID          int     `json:"person_id,omitempty"`
-	OrganizationID    int     `json:"organization_id,omitempty"`
-	Value             struct {
+	Title          string   `json:"title"`
+	OwnerID        int      `json:"owner_id,omitempty"`
+	LabelIDs       []string `json:"label_ids,omitempty"`
+	PersonID       int      `json:"person_id,omitempty"`
+	OrganizationID int      `json:"organization_id,omitempty"`
+	Value          struct {
 		Amount   float64 `json:"amount"`
 		Currency string  `json:"currency"`
 	} `json:"value,omitempty"`
-	ExpectedCloseDate string  `json:"expected_close_date,omitempty"`
-	VisibleTo         string  `json:"visible_to,omitempty"`
-	WasSeen           bool    `json:"was_seen,omitempty"`
-	OriginID          string  `json:"origin_id,omitempty"`
-	Channel           int     `json:"channel,omitempty"`
-	ChannelID         string  `json:"channel_id,omitempty"`
+	ExpectedCloseDate string `json:"expected_close_date,omitempty"`
+	VisibleTo         string `json:"visible_to,omitempty"`
+	WasSeen           bool   `json:"was_seen,omitempty"`
+	OriginID          string `json:"origin_id,omitempty"`
+	Channel           int    `json:"channel,omitempty"`
+	ChannelID         string `json:"channel_id,omitempty"`
 }
 
 type LeadUpdateRequest struct {
-	Title             string  `json:"title,omitempty"`
-	OwnerID           int     `json:"owner_id,omitempty"`
-	LabelIDs          []int   `json:"label_ids,omitempty"`
-	PersonID          int     `json:"person_id,omitempty"`
-	OrganizationID    int     `json:"organization_id,omitempty"`
-	IsArchived        bool    `json:"is_archived,omitempty"`
-	Value             struct {
+	Title          string `json:"title,omitempty"`
+	OwnerID        int    `json:"owner_id,omitempty"`
+	LabelIDs       []int  `json:"label_ids,omitempty"`
+	PersonID       int    `json:"person_id,omitempty"`
+	OrganizationID int    `json:"organization_id,omitempty"`
+	IsArchived     bool   `json:"is_archived,omitempty"`
+	Value          struct {
 		Amount   float64 `json:"amount"`
 		Currency string  `json:"currency"`
 	} `json:"value,omitempty"`
-	ExpectedCloseDate string  `json:"expected_close_date,omitempty"`
-	VisibleTo         string  `json:"visible_to,omitempty"`
-	WasSeen           bool    `json:"was_seen,omitempty"`
-	Channel           int     `json:"channel,omitempty"`
-	ChannelID         string  `json:"channel_id,omitempty"`
+	ExpectedCloseDate string `json:"expected_close_date,omitempty"`
+	VisibleTo         string `json:"visible_to,omitempty"`
+	WasSeen           bool   `json:"was_seen,omitempty"`
+	Channel           int    `json:"channel,omitempty"`
+	ChannelID         string `json:"channel_id,omitempty"`
 }
