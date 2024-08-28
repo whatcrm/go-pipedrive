83f9e63ac6a1d1f56b022c2d25fc3a52d4f8f805
Aisha Kozhamseitova <kozhamseitova91@gmail.com>
2024-08-28 19:08:49 +0500

    call logs fix

--- models/callLogs.go
+++ models/callLogs.go
@@ -31,18 +31,18 @@ type CallLog struct {
 }
 
 type CallLogReq struct {
-	ActivityID      int     `json:"activity_id"`
-	PersonID        *int    `json:"person_id"`
-	OrgID           *int    `json:"org_id"`
-	DealID          *int    `json:"deal_id"`
-	LeadID          *string `json:"lead_id"`
-	Subject         string  `json:"subject"`
-	Duration        string  `json:"duration"`
+	ActivityID      int     `json:"activity_id,omitempty"`
+	PersonID        *int    `json:"person_id,omitempty"`
+	OrgID           *int    `json:"org_id,omitempty"`
+	DealID          *int    `json:"deal_id,omitempty"`
+	LeadID          *string `json:"lead_id,omitempty"`
+	Subject         string  `json:"subject,omitempty"`
+	Duration        string  `json:"duration,omitempty"`
 	Outcome         string  `json:"outcome"` //required values: connected, no_answer, left_message, left_voicemail, wrong_number, busy
-	FromPhoneNumber string  `json:"from_phone_number"`
+	FromPhoneNumber string  `json:"from_phone_number,omitempty"`
 	ToPhoneNumber   string  `json:"to_phone_number"` //required 
 	StartTime       string  `json:"start_time"`//required 
 	EndTime         string  `json:"end_time"`//required 
-	UserID          int     `json:"user_id"`
-	Note            string  `json:"note"`
+	UserID          int     `json:"user_id,omitempty"`
+	Note            string  `json:"note,omitempty"`
 }
